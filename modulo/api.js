97782a3c1bf4a0663262a7488eddcff8207a4185
HIAGO RODRIGUES ORTOLAN <24122385@dsn127m1246726.cfp127aluno.local>
2025-08-07 17:24:04 -0300

    front

--- modulo/api.js
+++ modulo/api.js
@@ -2,7 +2,7 @@
 'use strict'
 
 async function pesquisarFotos() {
-    const url = `http://localhost:3000/fotos`
+    const url = `https://carrossel-back-6q9u.onrender.com/fotos`
     const response = await fetch(url)
     const fotos = await response.json()
     return fotos
